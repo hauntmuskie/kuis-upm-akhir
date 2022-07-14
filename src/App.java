a476e1645a2fafb10c94d5e4024b0fbe151f6246
hauntmuskie <rioclasher4@gmail.com>
2022-07-14 20:11:04 +0700

    fix logic

--- src/App.java
+++ src/App.java
@@ -7,73 +7,107 @@ import java.io.InputStreamReader;
  */
 public class App {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NumberFormatException {
         BufferedReader brInput = new BufferedReader(new InputStreamReader(System.in));
-        String isContinue, isCancel = "";
+        String isContinue, exitProgram = "", loopAgain;
         int menuChoice;
         
         do {
-            System.out.println("Silahkan Tentukan Pilihan Menu Yang Mau Anda Kerjakan");
-            System.out.print("Mulai Tentukan [y/T] ");
+            System.out.print("Silahkan Tentukan Pilihan Menu Yang Mau Anda Kerjakan\nMulai Tentukan [y/T] > ");
             isContinue = brInput.readLine();
 
             if (isContinue.equalsIgnoreCase("t")) {
                 System.out.println("Apakah anda yakin ingin mengakhiri program?");
-                System.out.print("Keluar [y/T] ");
-                isCancel = brInput.readLine();
+                System.out.print("Keluar [y/T] > ");
+                exitProgram = brInput.readLine();
 
-                if (isCancel.equalsIgnoreCase("t")) {
-                    System.out.println("Looping Kembali");
+                if (exitProgram.equalsIgnoreCase("t")) {
+                    System.out.println("Anda Membatalkan Keluar Dari Program..");
+                    System.out.println("Kembali Ke Menu Utama..");
 
-                } else if (isCancel.equalsIgnoreCase("y")) {
-                    System.out.println("Terima Kasih");
+                } else if (exitProgram.equalsIgnoreCase("y")) {
+                    System.out.println("Anda Keluar Dari Program..");
                     System.exit(0);
 
-                } else {
-                    System.out.println("Pilihan tidak diketahui");
-                    System.out.println("Looping Kembali");
-                }
-            } 
+                } else if (!(isContinue.equalsIgnoreCase("y") || isContinue.equalsIgnoreCase("n"))) {
+                    System.out.println("Pilihan tidak diketahui..\nHarap Masukkan Pilihan Dengan Benar!");
 
-            if (!(isContinue.equalsIgnoreCase("y") || isContinue.equalsIgnoreCase("n")) || isCancel.equalsIgnoreCase("t")) {
-                System.out.println("Pilihan tidak diketahui..\nHarap Masukkan Pilihan Dengan Benar!]");
+                }
             }
 
-        } while (!(isContinue.equalsIgnoreCase("y") || isContinue.equalsIgnoreCase("n")) || isCancel.equalsIgnoreCase("t"));
+        } while (!(isContinue.equalsIgnoreCase("y") || isContinue.equalsIgnoreCase("n")) || exitProgram.equalsIgnoreCase("t"));
 
         do {
-            System.out.println();
-            System.out.println("1. : Pilih Tarif Sewa Kendaraan");
-            System.out.println("2. : Pilih Bintang Besar Ke Kecil");
-            System.out.println("3. : Pilih Bintang Piramida");
-            System.out.println("4. : Pilih Data Penjualan Gamis");
-            System.out.println();
-            System.out.print("Masukan Pilihan Anda : ");
-            menuChoice = Integer.valueOf(brInput.readLine());
+            do {
+                System.out.println();
+                System.out.println("#===================================#");
+                System.out.println("|           Pilihan Menu            |");
+                System.out.println("#===================================#");
+                System.out.println("| 1. : Pilih Tarif Sewa Kendaraan   |");
+                System.out.println("#===================================#");
+                System.out.println("| 2. : Pilih Bintang Besar Ke Kecil |");
+                System.out.println("#===================================#");
+                System.out.println("| 3. : Pilih Bintang Piramida       |");
+                System.out.println("#===================================#");
+                System.out.println("| 4. : Pilih Data Penjualan Gamis   |");
+                System.out.println("#===================================#");
+                System.out.println();
+                System.out.print("Masukan Pilihan Anda : ");
+                menuChoice = Integer.valueOf(brInput.readLine());
+
+                if (menuChoice > 4) {
+                    System.out.println("\nInput Tidak Valid..\nHarap Masukkan Pilihan Dengan Benar!");
+                }
+
+            } while (menuChoice > 4);
     
             switch (menuChoice) {
-    
+
                 case 1:
                     System.out.println("1"); // Rionggo Rahardi
+
                     break;
-                
                 case 2:
                     System.out.println("2"); //
-    
+
                     break;
                 case 3: 
                     System.out.println("3"); //
-    
+
                     break;
                 case 4:
                     System.out.println("4"); // 
-    
+
                     break;
                 default:
-                    System.out.println("LMFAO"); // Turu
+                    System.err.println("LMFAO"); // Turu
+
                     break;
             }
-        } while (!(menuChoice == 1 || menuChoice == 2 || menuChoice == 3 || menuChoice == 4));
-        brInput.close();
+
+            do {
+                System.out.print("Ingin Mengulangi lagi? [y/T] > ");
+                loopAgain = brInput.readLine();
+
+                if (!(loopAgain.equalsIgnoreCase("y") || loopAgain.equalsIgnoreCase("n"))) {
+                    System.out.println("\nPilihan tidak diketahui..\nHarap Masukkan Pilihan Dengan Benar!\n");
+
+                }
+
+            } while (!(loopAgain.equalsIgnoreCase("y") || loopAgain.equalsIgnoreCase("n")));
+
+            if (loopAgain.equalsIgnoreCase("y")) {
+                System.out.println("Looping Kembali");
+
+            } else if (loopAgain.equalsIgnoreCase("t")) {
+                System.out.println("Terima Kasih");
+                System.exit(0);
+
+            }
+
+        } while (menuChoice > 4 || loopAgain.equalsIgnoreCase("y"));
+
+    brInput.close();
+
     }
 }
